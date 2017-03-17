0c6895b3b5991d14417acae0279dc56ef138bbad
Israel Lugo <hostelixisrael@gmail.com>
2017-03-17 00:19:57 -0400

    [MOD] modified file database.py to use env_vars file

--- database.py
+++ database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import json
 
-engine = create_engine('postgresql://postgres:canaima@localhost/movies', convert_unicode=True)
+
+def get_vars():
+    env_vars_file = open("env_vars.json", "r")
+    data_json = env_vars_file.read()
+
+    return json.loads(data_json)
+
+vars_env = get_vars()
+
+engine = create_engine(vars_env.get('postgres_uri'), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
